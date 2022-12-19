df4a04e7c201438d0caa8f27b02e7fe527fc5793
MaroVictor2022 <dreferaro2015@gmail.com>
2022-12-19 07:33:42 -0800

    My first C programming using printf function.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using printf function in C programming to print out a line of code.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
